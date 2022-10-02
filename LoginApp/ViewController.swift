27950f8662852582e81c88e711c8b16252a41d10
Albina <albina.exe@mail.ru>
2022-10-02 13:00:35 +0200

    wrote a code for forgot buttons

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func logInButtonWasTapped() {
+        
+    }
+    @IBAction func forgotUserNameButtonWasTapped() {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is Meow")
+    }
+    @IBAction func forgotPasswordButtonWasTapped() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is qwerty")
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 
 }
 
