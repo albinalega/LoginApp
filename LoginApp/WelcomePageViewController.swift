cbdc33abd9a0109a9ed2121b7f960beb6529041c
Albina <albina.exe@mail.ru>
2022-10-02 22:07:59 +0200

    finished but didn't do a gradient yet

--- LoginApp/WelcomePageViewController.swift
+++ LoginApp/WelcomePageViewController.swift
@@ -15,8 +15,25 @@ final class WelcomePageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+               
         welcomeUserLabel.text = welcomeUser
+        
+//        setGradientColor(colorOne: .purple, colorTwo: .yellow)
+        
+ 
     }
     
+//    func setGradientColor(colorOne: UIColor, colorTwo: UIColor) {
+//        let gradient: CAGradientLayer = CAGradientLayer()
+//
+//        gradient.colors = [colorOne.cgColor, colorTwo.cgColor]
+//        gradient.locations = [0.0, 1.0]
+//        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+//        gradient.endPoint = CGPoint(x: 0.0, y: 2.0)
+//        gradient.frame = view.bounds
+//        view.layer.addSublayer(gradient)
+//    }
+    
 }
+
+
